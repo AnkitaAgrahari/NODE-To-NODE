76034f2ff400bfde98f7ab2bf281c78d071052ff
ankita agrahari <ankitamanu08@gmail.com>
2022-07-27 12:32:09 +0530

    Successfully Done node to node communication

---
+++ STM32CubeExpansion_LRWAN_V2.1.0 - Copy/Projects/NUCLEO-L073RZ/Applications/SubGHz_Phy/SubGHz_Phy_PingPong/MDK-ARM/sx1262dvk1cas/Exe/radio.d
@@ -0,0 +1,62 @@
+sx1262dvk1cas/exe/radio.o: \
+  ..\..\..\..\..\..\Middlewares\Third_Party\SubGHz_Phy\sx126x\radio.c \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\math.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdbool.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  ..\SubGHz_Phy\Target\mw_log_conf.h \
+  ..\..\..\..\..\..\Utilities\trace\adv_trace\stm32_adv_trace.h \
+  ..\Core\Inc\utilities_conf.h \
+  ..\..\..\..\..\..\Drivers\CMSIS\Include\cmsis_compiler.h \
+  ..\..\..\..\..\..\Drivers\CMSIS\Include\cmsis_armclang.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  ..\..\..\..\..\..\Utilities\misc\stm32_mem.h \
+  ..\..\..\..\..\..\Utilities\misc\stm32_tiny_vsnprintf.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdarg.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  ..\SubGHz_Phy\Target\timer.h \
+  ..\..\..\..\..\..\Utilities\timer\stm32_timer.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h \
+  ..\..\..\..\..\..\Middlewares\Third_Party\SubGHz_Phy\radio.h \
+  ..\SubGHz_Phy\Target\radio_conf.h ..\Core\Inc\platform.h \
+  ..\..\..\..\..\..\Drivers\CMSIS\Device\ST\STM32L0xx\Include\stm32l0xx.h \
+  ..\..\..\..\..\..\Drivers\CMSIS\Device\ST\STM32L0xx\Include\stm32l073xx.h \
+  ..\..\..\..\..\..\Drivers\CMSIS\Include\core_cm0plus.h \
+  ..\..\..\..\..\..\Drivers\CMSIS\Include\cmsis_version.h \
+  ..\..\..\..\..\..\Drivers\CMSIS\Include\cmsis_compiler.h \
+  ..\..\..\..\..\..\Drivers\CMSIS\Include\mpu_armv7.h \
+  ..\..\..\..\..\..\Drivers\CMSIS\Device\ST\STM32L0xx\Include\system_stm32l0xx.h \
+  ..\Core\Inc\main.h \
+  ..\..\..\..\..\..\Drivers\STM32L0xx_HAL_Driver\Inc\stm32l0xx_hal.h \
+  ..\Core\Inc\stm32l0xx_hal_conf.h \
+  ..\..\..\..\..\..\Drivers\STM32L0xx_HAL_Driver\Inc\stm32l0xx_hal_rcc.h \
+  ..\..\..\..\..\..\Drivers\STM32L0xx_HAL_Driver\Inc\stm32l0xx_hal_def.h \
+  ..\..\..\..\..\..\Drivers\STM32L0xx_HAL_Driver\Inc\Legacy\stm32_hal_legacy.h \
+  ..\..\..\..\..\..\Drivers\STM32L0xx_HAL_Driver\Inc\stm32l0xx_hal_rcc_ex.h \
+  ..\..\..\..\..\..\Drivers\STM32L0xx_HAL_Driver\Inc\stm32l0xx_hal_gpio.h \
+  ..\..\..\..\..\..\Drivers\STM32L0xx_HAL_Driver\Inc\stm32l0xx_hal_gpio_ex.h \
+  ..\..\..\..\..\..\Drivers\STM32L0xx_HAL_Driver\Inc\stm32l0xx_hal_dma.h \
+  ..\..\..\..\..\..\Drivers\STM32L0xx_HAL_Driver\Inc\stm32l0xx_hal_exti.h \
+  ..\..\..\..\..\..\Drivers\STM32L0xx_HAL_Driver\Inc\stm32l0xx_hal_cortex.h \
+  ..\..\..\..\..\..\Drivers\STM32L0xx_HAL_Driver\Inc\stm32l0xx_hal_flash.h \
+  ..\..\..\..\..\..\Drivers\STM32L0xx_HAL_Driver\Inc\stm32l0xx_hal_flash_ex.h \
+  ..\..\..\..\..\..\Drivers\STM32L0xx_HAL_Driver\Inc\stm32l0xx_hal_flash_ramfunc.h \
+  ..\..\..\..\..\..\Drivers\STM32L0xx_HAL_Driver\Inc\stm32l0xx_hal_i2c.h \
+  ..\..\..\..\..\..\Drivers\STM32L0xx_HAL_Driver\Inc\stm32l0xx_hal_i2c_ex.h \
+  ..\..\..\..\..\..\Drivers\STM32L0xx_HAL_Driver\Inc\stm32l0xx_hal_pwr.h \
+  ..\..\..\..\..\..\Drivers\STM32L0xx_HAL_Driver\Inc\stm32l0xx_hal_pwr_ex.h \
+  ..\..\..\..\..\..\Drivers\STM32L0xx_HAL_Driver\Inc\stm32l0xx_hal_rtc.h \
+  ..\..\..\..\..\..\Drivers\STM32L0xx_HAL_Driver\Inc\stm32l0xx_hal_rtc_ex.h \
+  ..\..\..\..\..\..\Drivers\STM32L0xx_HAL_Driver\Inc\stm32l0xx_hal_spi.h \
+  ..\..\..\..\..\..\Drivers\STM32L0xx_HAL_Driver\Inc\stm32l0xx_hal_tim.h \
+  ..\..\..\..\..\..\Drivers\STM32L0xx_HAL_Driver\Inc\stm32l0xx_hal_tim_ex.h \
+  ..\..\..\..\..\..\Drivers\STM32L0xx_HAL_Driver\Inc\stm32l0xx_hal_uart.h \
+  ..\..\..\..\..\..\Drivers\STM32L0xx_HAL_Driver\Inc\stm32l0xx_hal_uart_ex.h \
+  ..\..\..\..\..\..\Drivers\BSP\STM32L0xx_Nucleo\stm32l0xx_nucleo.h \
+  ..\SubGHz_Phy\Target\nucleo_l073rz_bus.h \
+  ..\SubGHz_Phy\Target\mw_log_conf.h \
+  ..\SubGHz_Phy\Target\radio_board_if.h \
+  ..\..\..\..\..\..\Drivers\BSP\SX1262DVK1CAS\sx1262dvk1cas.h \
+  ..\SubGHz_Phy\Target\sx1262dvk1cas_conf.h ..\Core\Inc\utilities_def.h \
+  ..\Core\Inc\sys_debug.h ..\Core\Inc\sys_conf.h ..\Core\Inc\platform.h \
+  ..\..\..\..\..\..\Middlewares\Third_Party\SubGHz_Phy\sx126x\sx126x.h
---
+++ STM32CubeExpansion_LRWAN_V2.1.0 - Copy/Projects/NUCLEO-L073RZ/Applications/SubGHz_Phy/SubGHz_Phy_PingPong/MDK-ARM/sx1262dvk1cas/Exe/startup_stm32l073xx.d
@@ -0,0 +1,1 @@
+sx1262dvk1cas\exe\startup_stm32l073xx.o: startup_stm32l073xx.s
